13d0d4cfa7a2bfe0ffe05239e996a297582b96e3
Andy Hartford <andy.hartford@cohealo.com>
2015-01-26 21:18:49 -0500

    Add a badge for completed rounds

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,4 +1,14 @@
 class Round < ActiveRecord::Base
   belongs_to :game, touch: true
   has_many :plays, dependent: :destroy
+
+  # Return string representing average mean of all plays
+  def mean
+    plays.any? or return 'undefined'
+
+    sum = plays.inject(0) {|s, p| s + p.value}
+    mean = sum.to_f / plays.length.to_f
+
+    "%.1f" % mean
+  end
 end
