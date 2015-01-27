eed3b7d08589af08480ad8b07f7287411d3c78bc
Andy Hartford <andy.hartford@cohealo.com>
2015-01-26 21:49:14 -0500

    improve home page

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,4 @@
+class WelcomeController < ApplicationController
+  layout false
+  def home; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'games#new'
+  root to: 'welcome#home'
 
   resources :games do
     resources :events
