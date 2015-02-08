ec16f401c03baa0da9505c2a1051fc2ae1d5502f
Andy Hartford <andy.hartford@cohealo.com>
2015-02-08 16:59:31 -0500

    Add leave game button

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+  # returns html for a kick link to the given user
+  def kick_link(user)
+    is_kick = current_user != user
+    link_to (is_kick ? 'kick' : 'leave'),
+      game_user_path(@game, user),
+      'data-confirm' => 'are you sure?',
+      method: :delete,
+      remote: true,
+      class: "btn #{is_kick ? 'btn-danger' : 'btn-default'}"
+  end
 end
